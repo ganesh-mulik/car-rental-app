92ec3732df1dae190518d5023b78ac2a2cf7d1f4
Ganesh Prabhakar mulik <129154209+ganesh-mulik@users.noreply.github.com>
2024-10-21 22:40:32 +0530

    Login, dashboard component created

--- car-rental-app/src/app/pages/login/login.component.ts
+++ car-rental-app/src/app/pages/login/login.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  router = inject(Router);
 
+  loginForm:FormGroup = new FormGroup({
+    userName: new FormControl(""),
+    password: new FormControl("")
+  })
+
+  onLogin(){
+    const loginObj = this.loginForm.value;
+    if(loginObj.userName==="admin"&&loginObj.password==="123456"){
+      this.router.navigateByUrl("/dashboard");
+    }
+    else{
+      alert("Enter correct details");
+    }
+  }
 }
+
