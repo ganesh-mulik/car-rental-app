a4dc3be6108e825b2bb32983779bdb1bffe4f656
Ganesh Prabhakar mulik <129154209+ganesh-mulik@users.noreply.github.com>
2024-10-26 16:51:15 +0530

    Booking dreate and read done successfully

--- car-rental-app/src/app/pages/bookings/bookings.component.ts
+++ car-rental-app/src/app/pages/bookings/bookings.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component,inject,OnInit } from '@angular/core';
+import { BookingService } from '../../../services/booking.service';
+import { CarService } from '../../../services/car.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bookings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './bookings.component.html',
   styleUrl: './bookings.component.css'
 })
-export class BookingsComponent {
+export class BookingsComponent implements OnInit{
+  bookingService = inject(BookingService);
+  carService = inject(CarService);
+  bookingsData:any[] = [];
+  CarData:any[]=[];
 
+  bookingForm:FormGroup = new FormGroup({
+    CustomerName: new FormControl(""),
+    BookingDate: new FormControl(""),
+    MobileNo: new FormControl(""),
+    BookingId: new FormControl(0),
+    CarId: new FormControl(0),  
+    Email: new FormControl(""),
+    Discount: new FormControl(0),
+    TotalBillAmount: new FormControl(0),
+    CustomerCity:new FormControl(""),
+  });
+  
+  ngOnInit(): void {
+      this.getBookingData();
+      this.getCarData();
+  }
+
+  getBookingData(){
+    this.bookingService.getAllBookings().subscribe((res:any)=>{
+      this.bookingsData = res.data;
+    })
+  }
+
+  getCarData(){
+    this.carService.getAllCars().subscribe((res:any)=>{
+      this.CarData= res.data;
+    });
+  }
+
+  saveBookingData(){
+    const formData = this.bookingForm.value;
+    this.bookingService.createBooking(formData);
+    this.getBookingData();
+  }
+
+  // updateBookingsData(data:any){
+  //   this.bookingForm=new FormGroup({
+  //     CustomerName: new FormControl(data.CustomerName),
+  //     BookingDate: new FormControl(data.BookingDate),
+  //     MobileNo: new FormControl(data.MobileNo),
+  //     BookingId: new FormControl(data.BookingId),
+  //     CarId: new FormControl(data.CarId),  
+  //     Email: new FormControl(data.Email),
+  //     Discount: new FormControl(data.Discount),
+  //     TotalBillAmount: new FormControl(data.TotalBillAmount),
+  //     CustomerCity:new FormControl(data.CustomerCity),
+  //   });
+  // }
 }
---
+++ car-rental-app/src/services/booking.service.ts
@@ -0,0 +1,34 @@
+import { Injectable,inject,OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookingService{
+  apiUrl = "https://freeapi.gerasim.in/api/CarRentalApp/";
+  http = inject(HttpClient);
+
+  getAllBookings(){
+    return this.http.get(`${this.apiUrl}geAllBookings`);
+  }
+
+  createBooking(data:any){
+    this.http.post(`${this.apiUrl}CreateNewBooking`,data).subscribe((res:any)=>{
+      if(res.result){
+        return alert("Record inserted successfully");        
+      }else{
+        return alert(res.message);
+      }
+    })
+  }
+
+  updateBooking(data:any){
+    this.http.put(`${this.apiUrl}CreateNewBooking`,data).subscribe((res:any)=>{
+      if(res.result){
+        return alert("Record updated successfully");
+      }else{
+        return res.message;
+      }      
+    })
+  }
+}
